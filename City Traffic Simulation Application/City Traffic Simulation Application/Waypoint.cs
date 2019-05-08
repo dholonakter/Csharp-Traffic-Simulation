4e0a71b23890bf85e2e12f6aaa7c1079e427214b
Mahmud,Fahim F <f.mahmud@student.fontys.nl>
2019-05-08 13:42:22 +0200

    Revert "Added CrossingType1"
    This reverts commit d8f3a546750144728465ede00491cfccc8be2e2b

---
+++ City Traffic Simulation Application/City Traffic Simulation Application/Waypoint.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace City_Traffic_Simulation_Application
+{
+    [Serializable]
+  public  class Waypoint
+    {
+        //waypoints are a coordinate on the map. when the final waypoint is reached the entity will be sent to a Road.
+        public Waypoint nextWaypoint;
+        public Waypoint waypointStraight;
+        public Waypoint waypointLeft;
+        public Waypoint waypointRight;
+        
+
+        public double x;
+        public double y;
+
+        public Waypoint (double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Waypoint (double x, double y, Waypoint w)
+        {
+            this.x = x;
+            this.y = y;
+            this.nextWaypoint = w;
+            waypointStraight = w;
+            waypointLeft = w;
+            waypointRight = w;
+        }
+        public Waypoint (Point p, Waypoint w)
+        {
+            this.x = p.X;
+            this.y = p.Y;
+            this.nextWaypoint = w;
+            waypointStraight = w;
+            waypointLeft = w;
+            waypointRight = w;
+        }
+        public Waypoint(Point p)
+        {
+            this.x = p.X;
+            this.y = p.Y;
+        }
+    }
+}
